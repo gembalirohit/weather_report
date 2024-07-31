17cb069b232eed513b7fd82993380c2aa379703e
gembalirohit <gembalirohit2003@gmail.com>
2024-07-31 14:46:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+
+let weather = {
+    
+    fetchWeather : async function (city) {
+        await fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=metric&appid=" 
+        + "4f04dea0210d1927078354236048193a")
+        .then((Response) => Response.json())
+        .then((data) => this.displayWeather(data));
+    },
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },
+
+    search : function() {
+        this.fetchWeather(document.querySelector(".searchbar").value);
+    }
+};
+
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+});
+
+document.querySelector(".searchbar").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather("guntur");
